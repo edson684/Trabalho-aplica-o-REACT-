bceb608662feb017bffd9ca1692b5ff6005c632d
Edson Gabriel <hucklouco@gmail.com>
2026-04-28 21:22:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import APODGallery from './pages/APODGallery';
 import APODDetail from './pages/APODDetail';
-import Mars from './pages/Mars';
 import NEO from './pages/NEO';
 
 export default function App() {
@@ -15,7 +14,6 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/apod" element={<APODGallery />} />
           <Route path="/apod/:date" element={<APODDetail />} />
-          <Route path="/mars" element={<Mars />} />
           <Route path="/neo" element={<NEO />} />
         </Routes>
       </main>
